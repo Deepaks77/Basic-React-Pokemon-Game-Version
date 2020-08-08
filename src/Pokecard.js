2a44314bacc3b63d02351de5852541386b56d5f5
Deepak Sindhwani <dsindhwani55@gmail.com>
2020-08-08 19:29:12 +0530

    Before Introducing Pokegame Component

---
+++ src/Pokecard.js
@@ -0,0 +1,20 @@
+import React,{Component} from "react"
+import "./Pokecard.css"
+const Image_Api="https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+class Pokecard extends Component{
+   
+    render(){
+        const {id,name,type,exp}=this.props;
+        
+        return(
+            <div className="Pokecard">
+                <h1 className="Pokecard-title">{name}</h1>
+                <img src={Image_Api+id.toString().padStart(3,"0")+".png"} alt={name}/>
+                <div className="Pokecard-data">Type :{type}</div>
+                <div className="Pokecard-data"> Exp : {exp}</div>
+            </div>
+        )
+    }
+}
+
+export default Pokecard;
---
+++ src/Pokegame.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react'
+
+class Pokegame extends Component{
+    render(){
+        return(
+            <h1>Hello</h1>
+        )
+    }
+}
+
+export default Pokegame;
